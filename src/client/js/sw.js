09a2c2326f495127b23ca73946197a7ebdb0f2e5
Carlton Joseph <carlton.joseph@gmail.com>
2019-06-25 18:36:07 -0700

    minimum off line support added

--- src/client/js/sw.js
+++ src/client/js/sw.js
@@ -1,1 +1,24 @@
-console.log("sw 5 registered");
+const cacheName = "cache-v1";
+const resourcesToPrecache = ["/", "index.html", "index.js"];
+
+self.addEventListener("install", event => {
+  console.log("Install event");
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(resourcesToPrecache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  console.log("Activate event");
+});
+
+self.addEventListener("fetch", event => {
+  console.log("fetch intercepted for: ", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
